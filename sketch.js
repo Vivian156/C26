75f25b2ebe79fdaf41c5d6ef7f59188d91b679ef
Vivian <Vivloveslaramie@gmail.com>
2021-07-11 18:20:24 -0600

    added platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -18,6 +20,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
+    platform = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -34,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+
 
 }
 
@@ -59,4 +64,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
